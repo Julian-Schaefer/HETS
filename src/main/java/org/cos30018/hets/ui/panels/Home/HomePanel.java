791af4177ebf24a9dc68f36801575b85a17cad47
MalakaP95 <malakaperera887@gmail.com>
2018-10-14 18:24:10 +1100

    Added 'Energy Usage' Graph to homePanel

--- src/main/java/org/cos30018/hets/ui/panels/Home/HomePanel.java
+++ src/main/java/org/cos30018/hets/ui/panels/Home/HomePanel.java
@@ -1,9 +1,18 @@
 package org.cos30018.hets.ui.panels.Home;
 
 import net.miginfocom.swing.MigLayout;
+import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
 import org.jfree.data.xy.DefaultXYDataset;
 import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,7 +25,6 @@ public class HomePanel extends JPanel {
     private static final String SETTINGS_PANEL = "Settings Panel";
 
     public CardLayout homeLayout = new CardLayout();
-    ChartPanel cp;
     JButton btnSettings;
     JButton btnHome;
 
@@ -48,26 +56,101 @@ public class HomePanel extends JPanel {
         JPanel content = new JPanel(new MigLayout("center"));
         content.setPreferredSize(new Dimension(1280, 800));
 
-        JLabel title = new JLabel("Graph goes here");
-        content.add(title);
 
-        homeContentLayout.add(content);
-
-//        LineGraph();
-//
-//        homeContentLayout.add(cp);
-//        homeContentLayout.add(cp);
-
+        homeContentLayout.add(GraphChartPanel());
 
         return homeContentLayout;
     }
 
-//    private void LineGraph() {
-//        XYDataset ds = createDataset();
-//        JFreeChart chart = ChartFactory.createXYLineChart("Test Chart", "x", "y", ds, PlotOrientation.VERTICAL, true, true, false);
-//
-//        cp = new ChartPanel(chart);
-//    }
+    private JPanel GraphChartPanel() {
+        JPanel graphChartPanel = new JPanel(new MigLayout());
+        graphChartPanel.setPreferredSize(new Dimension(850, 600));
+
+        /* Title of the chart */
+        JLabel title = new JLabel("Energy Usage Actual vs Forecast");
+        title.setFont(title.getFont().deriveFont(20.0f));
+
+        graphChartPanel.add(title, "wrap");
+        graphChartPanel.add(Graph());
+
+
+        return graphChartPanel;
+    }
+
+    private JPanel Graph() {
+
+        JPanel chartContent = new JPanel();
+        XYDataset dataset = createDataset();
+        JFreeChart chart = createChart(dataset);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(700, 400));
+
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.blue);
+        chartContent.add(chartPanel);
+
+        return chartContent;
+    }
+
+
+    private XYDataset createDataset() {
+
+        XYSeries series1 = new XYSeries("Actual");
+        series1.add(18, 100);
+        series1.add(20, 130);
+        series1.add(25, 160);
+        series1.add(30, 250);
+
+        XYSeries series2 = new XYSeries("Forecast");
+        series2.add(18, 130);
+        series2.add(20, 160);
+        series2.add(25, 120);
+        series2.add(30, 300);
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series1);
+        dataset.addSeries(series2);
+
+        return dataset;
+    }
+
+
+    private JFreeChart createChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Forecast Energy usage",
+                "Time",
+                "Energy (Kwh)",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        XYPlot plot = chart.getXYPlot();
+
+        XYLineAndShapeRenderer renderer= new XYLineAndShapeRenderer();
+
+        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+
+        renderer.setSeriesPaint(1, Color.BLUE);
+        renderer.setSeriesStroke(1, new BasicStroke(2.0f));
+
+        plot.setRenderer(renderer);
+        plot.setBackgroundPaint(Color.white);
+
+        plot.setRangeGridlinesVisible(false);
+        plot.setDomainGridlinesVisible(false);
+
+        chart.getLegend().setFrame(BlockBorder.NONE);
+        chart.setTitle(new TextTitle("Forecast Energy usage", new Font("Serif", Font.BOLD, 18)));
+
+
+        return chart;
+    }
+
 
 
     public JPanel SettingsPanel(){
@@ -134,15 +217,5 @@ public class HomePanel extends JPanel {
     void addHomePanelListener(ActionListener listener) {
         btnSettings.addActionListener(listener);
         btnHome.addActionListener(listener);
-    }
-
-    private static XYDataset createDataset() {
-
-        DefaultXYDataset ds = new DefaultXYDataset();
-
-        double[][] data = { {0.1, 0.2, 0.3},{1, 2, 3} };
-        ds.addSeries("series1", data);
-
-        return ds;
     }
 }
