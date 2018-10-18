16d323ff0b5414e22cb3910fdf725976559cfae8
Julian Schäfer <julian.david.schaefer@gmail.com>
2018-10-18 15:42:22 +1100

    Negotiation Trial and Error

---
+++ src/main/java/org/cos30018/hets/negotiation/Negotiator.java
@@ -0,0 +1,8 @@
+package org.cos30018.hets.negotiation;
+
+import jade.lang.acl.ACLMessage;
+
+public abstract class Negotiator {
+
+	
+}
--- src/main/java/org/cos30018/hets/ui/appliance/AppliancePanel.java
+++ src/main/java/org/cos30018/hets/ui/appliance/AppliancePanel.java
@@ -1,6 +1,8 @@
 package org.cos30018.hets.ui.appliance;
 
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -10,14 +12,12 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 
 import org.cos30018.hets.ui.custom.AgentPanel;
 import org.cos30018.hets.ui.custom.JPanelList;
+import org.cos30018.hets.ui.custom.StyledButtonUI;
 
 import jade.core.AID;
-import org.cos30018.hets.ui.custom.StyledButtonUI;
-import org.cos30018.hets.ui.custom.StyledJPanelUI;
 
 public class AppliancePanel extends JPanel implements ActionListener {
 
@@ -28,23 +28,23 @@ public class AppliancePanel extends JPanel implements ActionListener {
 
 	private AppliancePanelController controller;
 	private AppliancePanelListener listener;
-	
+
 	private JButton addApplianceButton;
 	private JPanelList panelList;
-	
+
 	private Map<AID, JPanel> agentPanelForAID = new HashMap<>();
-	
+
 	public AppliancePanel() {
 		setLayout(new BorderLayout());
 		setup();
 		this.controller = new AppliancePanelController(this);
 	}
-	
+
 	private void setup() {
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 		buttonPanel.setBackground(Color.WHITE);
-		
+
 		addApplianceButton = new JButton("Add Appliance");
 		addApplianceButton.setFont(new Font("Raleway", Font.BOLD, 18));
 		addApplianceButton.setBackground(new Color(0x2dce98));
@@ -53,7 +53,7 @@ public class AppliancePanel extends JPanel implements ActionListener {
 		addApplianceButton.addActionListener(this);
 		buttonPanel.add(addApplianceButton);
 		buttonPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-		
+
 		add(buttonPanel, BorderLayout.NORTH);
 
 		panelList = new JPanelList();
@@ -64,21 +64,23 @@ public class AppliancePanel extends JPanel implements ActionListener {
 
 	/**
 	 * Function to ADDING an Appliance Agent
+	 * 
 	 * @param aid: appliance agent ID
 	 */
-	
-	public void addApplianceAgent(AID aid) {			
+
+	public void addApplianceAgent(AID aid) {
 		AgentPanel agentPanel = new AgentPanel(aid);
 		agentPanel.setAgentPanelListener(controller);
-        
+
 		agentPanelForAID.put(aid, panelList.addJPanel(agentPanel));
 	}
 
 	/**
 	 * Function to REMOVING an Appliance Agent
+	 * 
 	 * @param aid: appliance agent ID
 	 */
-	
+
 	public void removeApplianceAgent(AID aid) {
 		panelList.remove(agentPanelForAID.get(aid));
 		agentPanelForAID.remove(aid);
@@ -89,11 +91,11 @@ public class AppliancePanel extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		listener.onApplianceAddButtonClick();
 	}
-	
+
 	public void setAppliancePanelListener(AppliancePanelListener listener) {
 		this.listener = listener;
 	}
-	
+
 	public interface AppliancePanelListener {
 		void onApplianceAddButtonClick();
 	}
